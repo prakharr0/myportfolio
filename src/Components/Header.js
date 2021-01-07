03756b415db1c1b251ac269e8f72cc97e332d94b
Prakhar Rathi <prakhar@mail.uni-paderborn.de>
2021-01-07 16:58:35 +0100

    change in header --> more options

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
             <div className="main-info">
                 <h1>M.Sc. - Computer Science (Student)</h1>
                 <Typed className="typed-text" 
-                strings={["Java", "Full Stack Development", "Artificial Intelligence", "Natural Language Processing", "Machine Learning", "and much more..."]}
+                strings={["Java", "Web Development", "Android Application Development" , "Full Stack Development", "Artificial Intelligence", "Natural Language Processing", "Machine Learning", "and much more..."]}
                 typeSpeed={40}
                 backSpeed={60}
                 loop
